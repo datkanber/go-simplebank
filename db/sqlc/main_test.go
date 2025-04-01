0badebb3901a154063405de4c1f03401506e3192
datkanber <burak2kanber@gmail.com>
2025-04-01 18:03:50 +0300

    Unit testing go database CRUD

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	// Load .env from project root regardless of test directory
+	envPath := filepath.Join("..", "..", ".env")
+	if err := godotenv.Load(envPath); err != nil {
+		log.Printf("Warning: .env not loaded from %s, using system env vars", envPath)
+	}
+
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbPort := os.Getenv("POSTGRES_PORT")
+
+	dbSource := fmt.Sprintf(
+		"postgresql://%s:%s@localhost:%s/%s?sslmode=disable",
+		dbUser, dbPassword, dbPort, dbName,
+	)
+
+	conn, err := sql.Open("postgres", dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
