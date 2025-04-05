6e72b8be199db18eac3751854fd607f71384ca04
datkanber <burak2kanber@gmail.com>
2025-04-05 22:35:46 +0300

    hash password in go with bcrypt

---
+++ db/util/password_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	HashedPassword1, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, HashedPassword1)
+
+	err = CheckPassword(password, HashedPassword1)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, HashedPassword1)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	HashedPassword2, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, HashedPassword2)
+	require.NotEqual(t, HashedPassword1, HashedPassword2)
+}
